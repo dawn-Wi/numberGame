260ca6519630794655120bb42bd4dc96138dc48c
dawn <wi825@naver.com>
2022-07-19 15:34:31 +0900

    spinner delete

--- app/src/main/java/com/example/numbergame/game/GameSetting/GameSettingFragment.java
+++ app/src/main/java/com/example/numbergame/game/GameSetting/GameSettingFragment.java
@@ -41,19 +41,12 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class GameSettingFragment extends Fragment {
-    SharedPreferences sharedPreferences;
-    String locale;
-    int locale_number;
-    ArrayList<String> locales;
-    ArrayAdapter adapter;
 
     private FragmentGamesettingBinding binding;
     private GameSettingViewModel gameSettingViewModel;
 
-    private Spinner sp_language;
-    private TextView tv_maxNumber;
-    private TextView tv_orLabel;
     private EditText et_maxNumber;
+    private TextView tv_orLabel;
     private Button bt_gameStart;
     private Button bt_competition;
 
@@ -69,36 +62,7 @@ public class GameSettingFragment extends Fragment {
         return fragment;
     }
 
-    @NonNull
-    public static String getStringByLocal(Activity context, int resId, String locale) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-            return getStringByLocalPlus17(context, resId, locale);
-        else
-            return getStringByLocalBefore17(context, resId, locale);
-    }
 
-    @NonNull
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private static String getStringByLocalPlus17(Activity context, int resId, String locale) {
-        Configuration configuration = new Configuration(context.getResources().getConfiguration());
-        configuration.setLocale(new Locale(locale));
-        return context.createConfigurationContext(configuration).getResources().getString(resId);
-    }
-
-    private static String getStringByLocalBefore17(Context context, int resId, String language) {
-        Resources currentResources = context.getResources();
-        AssetManager assets = currentResources.getAssets();
-        DisplayMetrics metrics = currentResources.getDisplayMetrics();
-        Configuration config = new Configuration(currentResources.getConfiguration());
-        Locale locale = new Locale(language);
-        Locale.setDefault(locale);
-        config.locale = locale;
-
-        Resources defaultLocaleResources = new Resources(assets, metrics, config);
-        String string = defaultLocaleResources.getString(resId);
-        new Resources(assets, metrics, currentResources.getConfiguration());
-        return string;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -123,8 +87,6 @@ public class GameSettingFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentGamesettingBinding.inflate(inflater, container, false);
-        sp_language = binding.gameSettingSpLanguage;
-        tv_maxNumber = binding.gameSettingTvMaxNumberLabel;
         et_maxNumber = binding.gameSettingEtMaxNumber;
         tv_orLabel = binding.gamesettingTvOrLabel;
         bt_gameStart = binding.gameSettingBtGameStart;
@@ -138,70 +100,7 @@ public class GameSettingFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle saveInstanceState) {
         super.onViewCreated(view, saveInstanceState);
 
-        //region spinner
-        sharedPreferences = requireActivity().getSharedPreferences("shared", MODE_PRIVATE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            locale = sharedPreferences.getString("locale", getResources().getConfiguration().getLocales().get(0).getLanguage());
-        } else {
-            locale = sharedPreferences.getString("locale", Resources.getSystem().getConfiguration().locale.getLanguage());
-        }
-
-        switch (locale) {
-            case "ko": {
-                locale_number = 0;
-                break;
-            }
-            case "en": {
-                locale_number = 1;
-                break;
-            }
-        }
-        tv_maxNumber.setText(getStringByLocal(requireActivity(), R.string.tv_maxNumber, locale));
-
-        locales = new ArrayList<>();
-
-        locales.add(getStringByLocal(requireActivity(), R.string.korean, locale));
-        locales.add(getStringByLocal(requireActivity(), R.string.english, locale));
-
-        adapter = new ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_dropdown_item, locales);
-
-        sp_language.setAdapter(adapter);
-        sp_language.setSelection(locale_number);
-
-        sp_language.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                if (position != locale_number) {
-                    switch (position) {
-                        case 0: {
-                            locale = "ko";
-                            break;
-                        }
-                        case 1: {
-                            locale = "en";
-                            break;
-                        }
-                    }
-
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                    editor.putString("locale", locale);
-                    editor.commit();
-                    Intent intent = requireActivity().getBaseContext().getPackageManager()
-                            .getLaunchIntentForPackage(requireActivity().getBaseContext().getPackageName());
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    requireActivity().finish();
-                    startActivity(intent);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                sp_language.setSelection(locale_number);
-            }
-        });
-        //endregion
+        gameSettingViewModel.initGameFormState();
 
         //region viewModel
         gameSettingViewModel.getGameSettingFormState().observe(getViewLifecycleOwner(), new Observer<GameSettingFormState>() {
@@ -210,7 +109,6 @@ public class GameSettingFragment extends Fragment {
                 if (gameSettingFormState.getMaxNumberErrorMessage() != null) {
                     et_maxNumber.setError(gameSettingFormState.getMaxNumberErrorMessage());
                 }
-
                 gameSettingViewModel.setMaxNumberText(et_maxNumber.getText().toString());
                 bt_gameStart.setEnabled(gameSettingFormState.isFieldsValid());
             }
